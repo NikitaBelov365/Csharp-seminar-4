209d2e61a287cd4c358be7caa15336abd72e4dd7
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-22 13:35:35 +0300

    task 3 solved

---
+++ task 3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход 
+// число N и выдаёт произведение чисел от 1 до N.
+
+int InputInt(string prompt)
+{
+    System.Console.WriteLine($"{prompt}:");
+    if(int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("It's not a number");
+    Environment.Exit(1);
+    return 0;
+}
+
+int ProductNumbers(int value)
+{
+    int product = 1;
+    for (int i = 1; i <= value; i++)
+    {
+        product *=i;
+    }
+    return product;
+}
+
+int promptValue = InputInt("Enter number");
+System.Console.WriteLine($"Product of numbers from 1 to {promptValue} = {ProductNumbers(promptValue)}");
