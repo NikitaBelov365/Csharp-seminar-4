2252e9348cc3cc27b8991822b2c1b08c7eeb474a
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-22 11:24:17 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход 
+// число (А) и выдаёт сумму чисел от 1 до А.
+
+int InputInt(string msg)
+{
+    System.Console.WriteLine($"{msg}:");
+    int num;
+    if (int.TryParse(Console.ReadLine(), out num))
+    {
+        return num;
+    }
+    System.Console.WriteLine("It's not a number.");
+    Environment.Exit(1);
+    return 0;
+}
+
+int number = InputInt("Enter some number pls");
+int sum = 0;
+
+for (int i = 0; i <= number; i++)
+{
+    sum = sum + i;
+}
+
+System.Console.WriteLine(sum);
