1784e8f4c2b077f7707696a2788faeaab7c614db
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-22 12:58:07 +0300

    task 2 solved

---
+++ task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿int InputInt(string prompt)
+{
+    System.Console.WriteLine($"{prompt}:");
+    if(int.TryParse(Console.ReadLine(), out int value));
+    {
+        return value;
+    }
+    System.Console.WriteLine("It's not a number.");
+    Environment.Exit(1);
+    return 0;
+}
+
+int NumberLength(int value)
+{
+    int length = 0;
+    while (value > 0)
+    {
+        value /= 10;
+        length++;
+    }
+    return length;
+}
+
+int number = InputInt("Enter number");
+System.Console.WriteLine($"Length of number = {NumberLength(number)}");
