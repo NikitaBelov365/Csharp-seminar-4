5312d54da284657566a60ac60d4b88d9f22ee1ea
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-22 14:21:43 +0300

    task 4 solved

---
+++ task 4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3: Напишите программу, которая выводит массив из 
+// 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+int[] FillArray(int size)
+{
+    int [] myArray = new int[size];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i] = new Random().Next(0,2);
+    }
+    return myArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine();
+}
+
+int InputArraySize(string msg)
+{
+    System.Console.WriteLine(msg);
+    if(int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("It's not a number");
+    Environment.Exit(1);
+    return 0;
+}
+
+int sizeArray = InputArraySize("Enter array size:");
+int[] userArray = FillArray(sizeArray);
+PrintArray(userArray);
